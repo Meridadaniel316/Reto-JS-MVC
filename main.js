e5337f67d17676b4d09098e949a5320fd3e7cb85
Merida-Daniel <dacastamerd@gmail.com>
2021-05-26 13:13:01 -0500

    Se agrego el objeto ball

--- main.js
+++ main.js
@@ -11,13 +11,24 @@
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            //elements.push(this.ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 
 })();
-
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
 (function(){
     self.Bar = function(x,y,widht,height,board){
         this.x = x;
@@ -71,11 +82,16 @@
     }
 
     function draw(contexto,element){
-    console.log(element);
         switch(element.kind)
         {
             case "rectangle": 
                 contexto.fillRect(element.x,element.y,element.width,element.height);
+                break;
+            case "circle":
+                contexto.beginPath();
+                contexto.arc(element.x,element.y,element.radius,0,7);
+                contexto.fill();
+                contexto.closePath();
                 break;
         }
     }
@@ -87,6 +103,7 @@ var bar = new Bar(10,100,40,100,board);
 var bar_2 = new Bar(750,100,40,100,board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas,board);
+var ball = new Ball(400, 100, 10, board);
 
 document.addEventListener("keydown",function(ev){
     ev.preventDefault();
@@ -102,8 +119,6 @@ document.addEventListener("keydown",function(ev){
     else if(ev.keyCode == 83){
         bar.down();
     }
-
-    console.log(bar.toString());
 })
 
 
